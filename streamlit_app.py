997a8781c7ff79ac06cfcd6e424d1deaf627976a
Allan Shinhama <allanshinhama@gmail.com>
2024-10-15 18:02:48 -0300

    sdfg

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 
 import streamlit as st       # Biblioteca de Dashboard
 import pandas as pd          # Biblioteca de manipulação de dados
-#import plotly.express as px  # Biblioteca para criar tabelas e tals
+import plotly.express as px  # Biblioteca para criar tabelas e graficos
 
 # Objetivos do dashboard:
 
@@ -25,5 +25,14 @@ month = st.sidebar.selectbox("Mês", df["Month"].unique()) # Criando um sidebar 
 # df("Month").unique() Selecionando somente meses diferentes
 
 df_filtered = df[df["Month"] == month]
-# df_filtered Filtrando os meses
-df_filtered
+df_filtered # Filtrando os meses
+
+# CRIANDO OS GRAFICOS:
+
+# dividindo a tela em 2
+col1, col2 = st.columns(2)
+# dividindo a tela em 3
+col3, col4, col5 = st.columns(3)
+
+fig_date = px.bar(df_filtered, x="Date", y="Total", title="Faturamento por dia") # criando o grafico
+col1.plotly_chart(fig_date) # colocando o grafico de barras na coluna 1 do streamlit
